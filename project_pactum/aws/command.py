13e189c1852c159992595ffc3c50af0080bf0581
Jon Eyolfson <eyolfson@ucla.edu>
2020-10-09 14:04:56 -0400

    Added function to get public ips for latest image

--- project_pactum/aws/command.py
+++ project_pactum/aws/command.py
@@ -1,13 +1,9 @@
 import boto3
 
+from .instance import get_public_ips
+
 def test_command(options):
-	ec2 = boto3.client('ec2')
-	response = ec2.describe_instances()
-	for reservation in response['Reservations']:
-		for instance in reservation['Instances']:
-			if 'InstanceLifeCycle' in instance and instance['InstanceLifeCycle'] != 'spot':
-				continue
-			public_ip_address = 'OFFLINE'
-			if 'PublicIpAddress' in instance:
-				public_ip_address = instance['PublicIpAddress']
-			print(instance['InstanceId'], 'spot', public_ip_address)
+	ips = get_public_ips()
+	print('Public IPs')
+	for ip in ips:
+		print('-', ip)
