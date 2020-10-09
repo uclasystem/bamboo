4ebfab2a7859472954c4c2a68df9ac8871c5e3f4
Jon Eyolfson <eyolfson@ucla.edu>
2020-10-09 15:25:18 -0400

    Refactor aws.instance

--- project_pactum/aws/instance.py
+++ project_pactum/aws/instance.py
@@ -1,5 +1,21 @@
 import boto3
 
+def get_private_ips():
+	ips = []
+
+	ec2 = boto3.client('ec2')
+	response = ec2.describe_instances()
+	for reservation in response['Reservations']:
+		for instance in reservation['Instances']:
+			if 'SpotInstanceRequestId' not in instance:
+				continue
+			if instance['ImageId'] != 'ami-0676dd00c188dc297':
+				continue
+			if 'PublicIpAddress' not in instance:
+				continue
+			ips.append(instance['PrivateIpAddress'])
+	return ips
+
 def get_public_ips():
 	ips = []
 
