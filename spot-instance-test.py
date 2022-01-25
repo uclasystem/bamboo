d72d70afd213ce16e61622a2c96a23849ca54761
Jon Eyolfson <eyolfson@gmail.com>
2022-01-25 11:07:20 -0800

    Flush all testing output

--- spot-instance-test.py
+++ spot-instance-test.py
@@ -4,15 +4,15 @@ warned = False
 
 def handler(signum, frame):
     global warned
-    print('Got signal:', signum)
+    print('Got signal:', signum, flush=True)
     warned = True
 
 signal.signal(signal.SIGTERM, handler)
 
-print('Starting with PID:', os.getpid())
+print('Starting with PID:', os.getpid(), flush=True)
 while True:
     if warned:
-        print('Found warning')
+        print('Found warning', flush=True)
     else:
-        print('No warning')
+        print('No warning', flush=True)
     time.sleep(1)
