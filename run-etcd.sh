2e86333fac6d726a2337a899d0f56313e3d7e5ae
Jon Eyolfson <eyolfson@gmail.com>
2022-02-03 13:40:24 -0800

    Remove run-etcd.sh script

--- run-etcd.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-etcd --data-dir ~/etcd \
-    --enable-v2 \
-    --listen-client-urls http://0.0.0.0:2379 \
-    --advertise-client-urls http://0.0.0.0:2379 \
-    --initial-cluster-state new
